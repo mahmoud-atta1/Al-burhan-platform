2e978699e5ba851c43abc8c1f88ec8f31637b455
Mahmoud Atta <mahmoudatta484@gmail.com>
2026-05-11 01:06:23 +0300

    update project

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -44,6 +44,7 @@ exports.signup = asyncHandler(async (req, res, next) => {
   newUser.password = undefined;
   res.status(201).json({
     status: "success",
+    token,
     data: newUser,
   });
 });
@@ -70,6 +71,7 @@ exports.login = asyncHandler(async (req, res, next) => {
   user.password = undefined;
   res.status(200).json({
     status: "success",
+    token,
     data: user,
   });
 });
